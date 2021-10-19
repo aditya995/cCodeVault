e3f290cd9560b04a5a5dca0c51e4e8d049e36cf9
aditya995 <adityashowrov1996@gmail.com>
2021-10-19 22:00:32 +0600

    print.c added

--- core_concepts/print.c
+++ core_concepts/print.c
@@ -1,3 +1,13 @@
+/*
+g++ print.c
+filename.exe
+*/
+#include<stdio.h>
+
 int main(){
-    printf("gg");
+    int a;
+    printf("Give input integer\n");
+    scanf("%d", &a);
+    printf("%d is the input.", a);
+    return 0;
 }
