4abdd27bedf5cce619cf1bffa9f9985395bde05a
aditya995 <adityashowrov1996@gmail.com>
2021-10-22 07:51:49 +0600

    updated variables.c file

--- core_concepts/variables.c
+++ core_concepts/variables.c
@@ -11,6 +11,8 @@ int main(){
     char ca, cb;
 
     a = b = c = 78;
-    printf("a = %d b = %d c = %d", a, b, c);
+    printf("a = %d b = %d c = %d\n", a, b, c);
+    fa = fb = 88;
+    printf("fa = %d fb = %d\n", fa, fb);
     return 0;
 }
