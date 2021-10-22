fd8202c0c3aae88439ecfdc1a8d4f920e8842f46
aditya995 <adityashowrov1996@gmail.com>
2021-10-23 02:58:53 +0600

    updated variables.c file

--- core_concepts/variables.c
+++ core_concepts/variables.c
@@ -6,13 +6,22 @@ a.exe
 
 int main(){
     int a, b, c;
+    long a;
+    long long a;
+    long long int a;
+    unsigned long long a;
+    unsigned long long int a = 4;
     float fa, fb;
     double da, db;
     char ca, cb;
 
     a = b = c = 78;
-    printf("a = %d b = %d c = %d\n", a, b, c);
-    fa = fb = 88;
-    printf("fa = %d fb = %d\n", fa, fb);
+    printf("Integer -> a = %d b = %d c = %d\n", a, b, c);
+    fa = fb = 8.38;
+    printf("Float -> fa = %f fb = %f\n", fa, fb);
+    da = db = 88.2615;
+    printf("Double -> da = %f db = %f\n", da, db);
+    ca = cb = 'a';
+    printf("char -> ca = %c cb = %c\n", ca, cb);
     return 0;
 }
