bca651e9e6fc732eb9596114ea0f51758730019c
aditya995 <adityashowrov1996@gmail.com>
2021-10-24 07:25:57 +0600

    updated variables.c file

--- core_concepts/variables.c
+++ core_concepts/variables.c
@@ -5,23 +5,29 @@ a.exe
 #include<stdio.h>
 
 int main(){
-    int a, b, c;
-    long a;
-    long long a;
-    long long int a;
-    unsigned long long a;
-    unsigned long long int a = 4;
+    int ia, ib, ic;
+    long la;
+    long long lla;
+    long long int llia;
+    unsigned long long ulla;
+    unsigned long long int ullia;
     float fa, fb;
     double da, db;
     char ca, cb;
 
-    a = b = c = 78;
-    printf("Integer -> a = %d b = %d c = %d\n", a, b, c);
+    ia = ib = ic = 78;
+    printf("|size %d| Integer -> ia = %d ib = %d ic = %d\n",sizeof(ia), ia, ib, ic);
+    la = 15;
+    lla = 15;
+    llia = 15;
+    ulla = 15;
+    ullia = 15;
+    printf("|size %d| Long a -> la = %ld \n|size %d| Long Long a = %lld \n|size %d| Long Long Integer ia = %lld \n|size %d| Unsigned Long Long a = %llu \n|size %d| Unsigned Long Long Integer a = %llu \n", sizeof(la), la, sizeof(lla), lla, sizeof(llia) , llia, sizeof(ulla), ulla, sizeof(ullia), ullia);
     fa = fb = 8.38;
-    printf("Float -> fa = %f fb = %f\n", fa, fb);
+    printf("|size %d| Float -> fa = %f fb = %f\n",sizeof(fa), fa, fb);
     da = db = 88.2615;
-    printf("Double -> da = %f db = %f\n", da, db);
+    printf("|size %d| Double -> da = %f db = %f\n",sizeof(da), da, db);
     ca = cb = 'a';
-    printf("char -> ca = %c cb = %c\n", ca, cb);
+    printf("|size %d| char -> ca = %c cb = %c\n", sizeof(ca), ca, cb);
     return 0;
 }
